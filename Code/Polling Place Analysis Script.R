26b3aed0aaaba24895e2fadc765386827bf3800a
Justin Schulberg <schulbergjustin@gmail.com>
2020-12-13 10:50:43 -0500

    Developed maps for each year and combined it with the bar chart plot

--- Code/Polling Place Analysis Script.R
+++ Code/Polling Place Analysis Script.R
@@ -22,12 +22,14 @@ suppressMessages(library("pacman"))
 pacman::p_load("tidyverse", # Used for data wrangling,
                "tidyr", # Used for data cleaning,
                "ggplot2", # Used for visualizations,
+               "here", # Used for navigating project structure
                "maps", # Used for map-based visualizations
                "readxl", # Used for loading excel files,
                "readr", # Used for working with files,
                "pander", # Used for pretty tables,
                "kableExtra", # Used for RMarkdown formatting
                "lubridate", # Used for fixing dates,
+               "usmap", # Used for plotting US states
                "praise", # Used for positive reinforcement,
                "janitor", # Used for data cleaning,
                "pdftools", # Used for reading PDF files in,
@@ -65,36 +67,37 @@ polling_joined <- polling_places %>%
 # Let's start by finding our 10 states which have the most polling
 # places in each year, normalized by population
 pp_counts <- polling_joined %>%
-  group_by(state, year, population) %>%
+  group_by(state, state_abb, year, population) %>%
   summarise(polling_sites = n()) %>%
   mutate(
     # Calculate our percent change YoY
     percent_change = round(100*((polling_sites - lag(polling_sites)) / lag(polling_sites)), 2),
     # Pull in our population data to calculate the per capita rate of polling places
-    ps_per_capita = 1000000 * polling_sites / population
+    ps_per_capita = round(1000000 * polling_sites / population, 0)
     ) %>%
   print()
 
 # Now that we have our dataset, let's build 4 scatter plots (one for each year)
 # and look for any outliers.
-pp_counts %>%
+pp_scatter <- pp_counts %>%
 ggplot(aes(x = polling_sites, y = population)) +
   # Make it a scatter plot
-  geom_point(color = "slateblue", alpha = .8) +
+  geom_point(color = "slateblue", alpha = .8, size = 2) +
   geom_text(aes(label = state), # label by state
             color = "slateblue", # Make our color match
-            size = 3, # shrink the size
-            alpha = .8, # add some transparency
-            check_overlap = T) # avoid overlabelling
-            # nudge_y = 150) + # nudge the text a bit off center
+            size = 4, # shrink the size
+            alpha = .9, # add some transparency
+            check_overlap = T, # avoid overlabelling
+            nudge_y = 500000) + # nudge the text a bit off center
   theme_classic() +
   # Create separate scatter plots for each year
   facet_wrap(~ year) +
   # Let's change the names of the axes and title
   labs(title = "Number of Polling Places by Population",
-       subtitle = paste("Data is broken out across", n_distinct(pp_counts$state), "states."),
+       subtitle = paste("*Data broken out across", n_distinct(pp_counts$state), "states,",
+                        "represents the number of polling places per 1,000,000 people."),
        caption = paste("Data is accredited to the work of the Center for Public Integrity\n",
-                     "https://github.com/publici/us-polling-places")
+                       "https://github.com/publici/us-polling-places")
        ) +
   xlab("Number of Polling Sites") +
   ylab("Population") +
@@ -102,3 +105,80 @@ ggplot(aes(x = polling_sites, y = population)) +
   theme(plot.title = element_text(hjust = 0, color = "slateblue4"),
         plot.subtitle = element_text(color = "slateblue1", size = 10),
         plot.caption = element_text(hjust = 1, face = "italic", color = "dark gray"))
+
+ggsave(here("Viz/Polling Places Scatter Plot.jpg"), pp_scatter)
+
+# Let's try two more visualizations to help look for patterns
+# Start by creating a dataframe of our top 10 states per year
+top_10_pp <- pp_counts %>%
+  # Group by level and state
+  group_by(year) %>%
+  # Pick our top 10
+  top_n(10, ps_per_capita) %>%
+  # Rearrange our dataset
+  arrange(year, desc(ps_per_capita)) %>%
+  ungroup() %>%
+  print()
+
+
+pp_bar <- top_10_pp %>%
+  # Start our visualization, creating our groups by party affiliation
+  ggplot(aes(x = ps_per_capita,
+             # Reorder the variable so they are arrange descending for each year plot
+             y = forcats::fct_reorder(paste(state, year, sep = "_"), ps_per_capita)
+             )) +
+  geom_bar(stat = "identity", fill = "slateblue", na.rm = T) +
+  # Create a separate chart, with a flexible y-axis, for each level of office
+  facet_wrap(~year, scales = "free_y") +
+  # Add a label by recreating our data build from earlier
+  geom_label(aes(label = paste(state_abb, ps_per_capita, sep = "-")),
+             size = 3,
+             # Scooch the labels over a smidge
+             hjust = .25) +
+  # Change the theme to classic
+  theme_classic() +
+  # Let's change the names of the axes and title
+  xlab("Polling Sites per Capita*") +
+  ylab("States") +
+  labs(title = "Number of Polling Places by Population",
+       subtitle = paste("*Data broken out across", n_distinct(pp_counts$state), "states,",
+                        "represents the number of polling places per 1,000,000 people."),
+       caption = paste("Data is accredited to the work of the Center for Public Integrity\n",
+                       "https://github.com/publici/us-polling-places")
+  ) +
+  # format our title and subtitle
+  theme(plot.title = element_text(hjust = 0, color = "slateblue4"),
+        plot.subtitle = element_text(hjust = 0, color = "slateblue2", size = 10),
+        plot.caption = element_text(color = "dark gray", size = 10, face = "italic"),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank())
+
+ggsave(here("Viz/Top Polling Places Bar Chart.jpg"), pp_bar)
+
+
+# Let's take a look at the same data on a map to look for differences
+# geographically.
+plot_usmap(data = pp_counts,
+           values = "ps_per_capita",
+           color = "gray",
+           # Only include states in our dataset
+           include = unique(pp_counts$state_abb)) +
+  scale_fill_continuous(name = "Polling Sites per Capita",
+                        label = scales::comma,
+                        low = "white",
+                        high = "slateblue",
+                        # Manually set the limits so its easier to see differences
+                        limits = c(0, 1500)) +
+  facet_wrap(~ year) +
+  labs(title = "Number of Polling Places by Population",
+       subtitle = paste("*Data broken out across", n_distinct(pp_counts$state), "states,",
+                        "represents the number of polling places per 1,000,000 people."),
+       caption = paste("Data is accredited to the work of the Center for Public Integrity\n",
+                       "https://github.com/publici/us-polling-places")
+  ) +
+  theme(legend.position = "right",
+        plot.title = element_text(hjust = 0, color = "slateblue4"),
+        plot.subtitle = element_text(hjust = 0, color = "slateblue2", size = 10),
+        plot.caption = element_text(color = "dark gray", size = 10, face = "italic"),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank())
